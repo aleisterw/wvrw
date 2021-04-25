b09eb1afa06cb1586dc5dfb80df55979658c1234
AW <aw@lainia.net>
2021-04-25 17:03:29 +0800

    add dirs

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI(docs_url=None, redoc_url=None)
 
+app = FastAPI(docs_url="/docs", redoc_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{item_id}", response_class=HTMLResponse)
+async def read_item(request: Request, item_id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "item_id": item_id})
 
 @app.get("/")
 async def root():
